c07514a0a6725e3669883385e59fe42d3d56d0a0
Tseso <tserensodnom.enkh@gmail.com>
2021-11-24 17:17:40 +0800

    Added the about API

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,10 @@
+const dateRegex = new RegExp('^\\d\\d\\d\\d-\\d\\d-\\d\\d');
+function jsonDateReviver(key, value) {
+  if (dateRegex.test(value)) return new Date(value);
+  return value;
+}
+
+
 class IssueFilter extends React.Component {
   render() {
     return (
@@ -15,20 +22,11 @@ function IssueRow(props) {
       <td>{issue.owner}</td>
       <td>{issue.created.toDateString()}</td>
       <td>{issue.effort}</td>
-      <td>{issue.due ? issue.due.toDateString() : ''}</td>
+      <td>{issue.due ? issue.due.toDateString() : ' '}</td>
       <td>{issue.title}</td>
     </tr>);
 }
-const initialIssues = [
-  {
-    id: 1, status: 'New', owner: 'Ravan', effort: 5,
-    created: new Date('2018-08-15'), due: undefined,
-    title: 'Error in console when clicking Add',
-  }, {
-    id: 2, status: 'Assigned', owner: 'Eddie', effort: 14,
-    created: new Date('2018-08-16'), due: new Date('2018-08-30'),
-    title: 'Missing bottom border on panel',
-  },];
+
 function IssueTable(props) {
   const issueRows = props.issues.map(issue =>
     <IssueRow key={issue.id} issue={issue} />
@@ -85,10 +83,22 @@ class IssueList extends React.Component {
   componentDidMount() {
     this.loadData();
   }
-  loadData() {
-    setTimeout(() => {
-      this.setState({ issues: initialIssues });
-    }, 500);
+  async loadData() {
+    const query = `query {
+      issueList {
+        id title status owner
+        created effort due
+      }
+    }`;
+
+    const response = await fetch('/graphql', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ query })
+    });
+    const body = await response.text();
+    const result = JSON.parse(body, jsonDateReviver);
+    this.setState({ issues: result.data.issueList });
   }
   createIssue(issue) {
     issue.id = this.state.issues.length + 1;
